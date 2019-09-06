82ad98ca00655437f7bdfcf198010266eff00e66
panduwijayasaputra <panduwijayasaputra@gmail.com>
2019-09-07 02:02:12 +0700

    v1

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-  console.log("ready");
+  $("#hamburger").click(function() {
+    $(".hamburger__bar").toggleClass("active");
+    $("#mobile-nav").toggleClass("active");
+  });
+
+  $(".nav--mobile__dropdown").click(function() {
+    $(".nav--mobile__dropdown ul").slideDown({
+      start: function() {
+        jQuery(this).css("display", "flex");
+      }
+    });
+    $(".nav--mobile__dropdown > a").css("display", "none");
+  });
+
+  $(".nav--mobile__dropdown")
+    .siblings()
+    .click(function() {
+      $(".nav--mobile__dropdown ul").slideUp({
+        start: function() {
+          jQuery(".nav--mobile__dropdown > a").css("display", "flex");
+        }
+      });
+
+      $(".nav--mobile__dropdown > a").slideDown({
+        start: function() {
+          jQuery(this).css("display", "flex");
+        }
+      });
+    });
 });
